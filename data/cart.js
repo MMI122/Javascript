9d0d1e7fa86bbbeaff7e12bba16ae86937811e55
MMI122 <mubinislam05@gmail.com>
2025-04-12 03:15:46 +0600

    Make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -53,3 +53,17 @@ export function removeFromCart(productId){
   saveToStorage();
 
 }
+export function updateDeliveryOption(productId, deliveryOptionID){
+  let matchingItem;
+  cart.forEach((cartItem)=>{
+    if(productId===cartItem.productId){
+      matchingItem=cartItem;
+
+    }
+
+  });
+  matchingItem.deliveryOptionId=deliveryOptionID;
+  saveToStorage();
+  
+
+}
